f1fc6cae1404f39fd9deba5265666eb8a257303e
rahayuadiela10 <rahayuadielarazade2005@gmail.com>
2025-05-08 18:47:57 +0700

    User Dapat melihat kegiatan yang sudah ditambahkan

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h2>Daftar Kegiatan</h2>
+    <ul v-if="getTodosToShow.length > 0">
+      <li
+        v-for="(item, idx) in getTodosToShow"
+        :key="idx"
+      >
+        {{ item.text }}
+      </li>
+    </ul>
+    <p v-else>Tidak ada kegiatan yang terdaftar.</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { ref, computed } from 'vue'
+
+const todoList = ref([
+  { text: 'Belajar VueJS', completed: false },
+  { text: 'Mengerjakan Tugas UTS', completed: true }
+])
+
+const getTodosToShow = computed(() => todoList.value)
+</script>
